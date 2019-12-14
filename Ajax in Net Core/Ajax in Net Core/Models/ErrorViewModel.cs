dddf49851ffa610db40df11099c512a61ef43a69
PhanNam98 <43126684+PhanNam98@users.noreply.github.com>
2019-12-14 14:16:53 +0700

    'init'

---
+++ Ajax in Net Core/Ajax in Net Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ajax_in_Net_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
